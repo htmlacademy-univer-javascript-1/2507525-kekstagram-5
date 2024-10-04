ea9f26f253111aa0c3aca8659aaa1646affeaafd
Dmitriy <demix.antonov@yandex.ru>
2024-10-05 00:32:41 +0500

    Добавляет фунцию создания галереи с комментариями пользователей

--- js/functions.js
+++ js/functions.js
@@ -9,9 +9,9 @@ const checkLengthString = function(phrase, maxLength){
 const checkPalindrome = function(phrase){
   let normalString = phrase.replaceAll(' ','');
   normalString = normalString.toUpperCase();
-  let newPhrase='';
-  for (let i=normalString.length-1; i>=0;i--){
-    newPhrase+=normalString[i];
+  let newPhrase = '';
+  for (let i = normalString.length - 1; i >= 0;i--){
+    newPhrase += normalString[i];
   }
   if (newPhrase === normalString){
     return true;
@@ -19,3 +19,5 @@ const checkPalindrome = function(phrase){
   return false;
 };
 
+checkLengthString();
+checkPalindrome();
