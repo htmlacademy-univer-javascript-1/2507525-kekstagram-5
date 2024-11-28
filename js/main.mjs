919e112eba921b541ce0d7f20b66e9df3fd2df2a
Dmitriy <demix.antonov@yandex.ru>
2024-11-28 19:56:58 +0500

    9.13. Правда или действие

--- js/main.mjs
+++ js/main.mjs
@@ -1,5 +1,10 @@
 import { generatePhotos } from './data.mjs';
 import { renderPhotos } from './renderPhotos.mjs';
+import { initForm } from './formValidation.mjs';
 
 const photos = generatePhotos();
 renderPhotos(photos);
+
+document.addEventListener('DOMContentLoaded', () => {
+  initForm();
+});
